a9d8374f077b5160ddbee748f3563fbb56c602a9
MitjaDragan <mitja.dragan@gmail.com>
2017-03-14 12:22:00 +0100

    Improved design and added alltimebest.

--- app/src/main/java/com/example/mitja/react/React.java
+++ app/src/main/java/com/example/mitja/react/React.java
@@ -1,6 +1,7 @@
 package com.example.mitja.react;
 
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,8 +12,9 @@ import java.util.Random;
 
 public class React extends AppCompatActivity {
     int counter = 0;
-    int konec = 0;
+    int end = 0;
     int best = 10000;
+    int currentBest = 10000;
 
     View view;
 
@@ -26,8 +28,13 @@ public class React extends AppCompatActivity {
         final TextView label2 = (TextView) findViewById(R.id.textView2);
         final TextView ada = (TextView) findViewById(R.id.textView3);
         final TextView label3 = (TextView) findViewById(R.id.textView4);
+        final TextView label4 = (TextView) findViewById(R.id.textView5);
         final Button clicker = (Button) findViewById(R.id.button);
         final Button druga = (Button) findViewById(R.id.button2);
+
+
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "BEBAS___.ttf");
+        label.setTypeface(myTypeface);
 
         view = this.getWindow().getDecorView();
         view.setBackgroundResource(R.color.druga);
@@ -39,15 +46,20 @@ public class React extends AppCompatActivity {
             label3.setVisibility(View.INVISIBLE);
         }
 
-        label3.setText("Best time: " + Integer.toString(best));
+        label3.setText("All time best: " + Integer.toString(best));
 
         clicker.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(final View v) {
+                label.setTextColor(getResources().getColor(R.color.bluet));
+                label2.setTextColor(getResources().getColor(R.color.bluen));
+                label4.setTextColor(getResources().getColor(R.color.bluen));
+                ada.setTextColor(getResources().getColor(R.color.bluen));
+                label3.setTextColor(getResources().getColor(R.color.bluen));
                 clicker.setVisibility(View.INVISIBLE);
                 counter = 0;
-                final int num = rand.nextInt(60 - 35) + 35;
+                final int num = rand.nextInt(60 - 25) + 25;
                 label.setVisibility(View.VISIBLE);
                 label2.setText("Current time: 0");
                 ada.setText("Press to start!");
@@ -60,6 +72,8 @@ public class React extends AppCompatActivity {
                         if (counter >= num){
                             view.setBackgroundResource(R.color.zelena);
                             label.setVisibility(View.VISIBLE);
+
+                            label.setTextColor(getResources().getColor(R.color.greent));
                         }
                         druga.setOnClickListener(new View.OnClickListener() {
 
@@ -79,39 +93,54 @@ public class React extends AppCompatActivity {
                         counter++;
                     }
                     public void onFinish() {
+                        label.setTextColor(getResources().getColor(R.color.bluet));
                         ada.setText("Press to start!");
                         clicker.setVisibility((View.VISIBLE));
                         view.setBackgroundResource(R.color.druga);
                         label.setText("Start React");
-                        konec = counter - num;
-                        label2.setText("Current time: " + String.valueOf(konec));
+                        end = counter - num;
+                        label2.setText("Current time: " + String.valueOf(end));
                         druga.setVisibility(View.INVISIBLE);
-                        if (konec<=best){
-                            label3.setText("Best time: " + konec);
-                            best = konec;
+                        if (end<=best){
+                            label3.setText("All time best " + end);
+                            best = end;
                             SharedPreferences preferences = getSharedPreferences("MyPreferences", 0);
                             preferences.edit().putInt("best_time", best).apply();
                             label3.setVisibility(View.VISIBLE);
                         }
                         else{
-                            label3.setText("Best time: " + best);
+                            label3.setText("All time best: " + best);
                         }
+
+                        if (end<=currentBest){
+                            label4.setText("Current best: " + end);
+                            currentBest = end;
+                        }
+                        else{
+                            label4.setText("Current best: " + currentBest);
+                        }
+
                     }
                     public void onFinish2(){
+                        label.setTextColor(getResources().getColor(R.color.redt));
+                        label2.setTextColor(getResources().getColor(R.color.redn));
+                        label4.setTextColor(getResources().getColor(R.color.redn));
+                        ada.setTextColor(getResources().getColor(R.color.redn));
+                        label3.setTextColor(getResources().getColor(R.color.redn));
                         ada.setText("Press to start!");
                         clicker.setVisibility((View.VISIBLE));
                         view.setBackgroundResource(R.color.prva);
                         label.setText("Start React");
                         label2.setText("TOO FAST BOIIIIIII");
                         druga.setVisibility(View.INVISIBLE);
-                        if (konec<=best){
-                            label3.setText("Best time: " + konec);
-                            best = konec;
+                        if (end<=best){
+                            label3.setText("All time best: " + end);
+                            best = end;
                             SharedPreferences preferences = getSharedPreferences("MyPreferences", 0);
                             preferences.edit().putInt("best_time", best).apply();
                         }
                         else{
-                            label3.setText("Best time: " + best);
+                            label3.setText("All time best: " + best);
                         }
                     }
                 }.start();
